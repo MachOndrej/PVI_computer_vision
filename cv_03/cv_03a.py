56b8a16e05d0e7cd42acf07cdc88feaf34a395db
MachOndrej <ondrej.mach3@tul.cz>
2023-10-02 18:23:52 +0200

    CV3 color picker

--- cv_03/cv_03a.py
+++ cv_03/cv_03a.py
@@ -1,11 +1,13 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 im = cv2.imread('cv02_02.bmp')
-
 im2 = im.copy()
-hsv = cv2.cvtColor(im2, cv2.COLOR_BGR2HSV)
-gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
 rows, cols = gray.shape
 
@@ -18,7 +20,7 @@ def rotate(image, angle, center=None, scale=1.0):
     rotated = cv2.warpAffine(image, M, (w, h))
     return rotated
 
-
+#TODO: Psani do obrazku
 def write_to_image(im, org, text, text_color='black'):
     # font
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -33,6 +35,33 @@ def write_to_image(im, org, text, text_color='black'):
     thickness = 2
     cv2.putText(im, text, org, font, fontScale, colT, thickness, cv2.LINE_AA)
 
+#TODO: vraceni array pro porovnani
+def color_picker(image):
+    height, width, channels = image.shape
+    black_array = np.array([0, 0, 0])
+    # Loop through all pixels
+    for y in range(height):
+        for x in range(width):
+            # Get the color of the pixel at position (x, y)
+            pixel_color = image[y, x]
+            # Check if the arrays are equal
+            if not np.array_equal(pixel_color, black_array):
+                print(pixel_color)
+                return       # Arrays are not equal
+
+#TODO: prepsat do list of arrays pro lepsi vyber
+"""
+[255 128 200] pink
+[127   0 127] purple
+[255 127   0] orange
+[255 242   0] yellow
+[  0   0 255] blue
+[  0 255   0] gree
+[255   0   0] red 
+[127   0 127] purple
+[255 128 200] pink
+"""
+
 
 def crop_img_part(img):
     part_num = 0
@@ -40,15 +69,16 @@ def crop_img_part(img):
     for x in range(50, rows, 100):
         for y in range(50, cols, 100):
             crpd_img = img[x-50:x+50, y-50:y+50]
+            color_picker(crpd_img)
             rotated_crpd_img = rotate(crpd_img, angle_val)
             angle_val += 45
             img[x-50:x+50, y-50:y+50] = rotated_crpd_img
-            plt.title('IMage Cropped' + str(part_num))
-            plt.imshow(rotated_crpd_img)
-            plt.show()
+            #plt.title('IMage Cropped' + str(part_num))
+            #plt.imshow(rotated_crpd_img)
+            #plt.show()
 
 
 crop_img_part(im)
-plt.title('IMage ')
+plt.title('IMage After')
 plt.imshow(im)
 plt.show()
