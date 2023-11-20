a4cc6b09f02b6e6fbb7feb1b4739b2c0100e9342
MachOndrej <ondrej.mach3@tul.cz>
2023-11-20 21:29:58 +0100

    cv9 - done

--- cv_9/cv_09.py
+++ cv_9/cv_09.py
@@ -1,14 +1,30 @@
 from __future__ import print_function
 import numpy as np
 import cv2 as cv
-import argparse
 from matplotlib import pyplot as plt
+from skimage import transform
+import easyocr
+
+
+def cut_out_rectangle(img, dst):
+    mask = np.zeros_like(img, dtype=np.uint8)
+    cv.fillPoly(mask, [np.int32(dst)], 255)
+    result = cv.bitwise_and(img, mask)
+    return result
+
+
+def cutout_face_name_surname(img):
+    face = img[140:420, 15:230]
+    name = img[108:132, 150:350]
+    surename = img[82:110, 150:350]
+    return face, name, surename
 
 
 def first_method():
     MIN_MATCH_COUNT = 10
     img1 = cv.imread('pvi_cv09/obcansky_prukaz_cr_sablona_2012_2014.png', cv.IMREAD_GRAYSCALE)      # queryImage
     img2 = cv.imread('pvi_cv09/CA10_01.jpg', cv.IMREAD_GRAYSCALE)                                   # trainImage
+    img_positioned = img2.copy()
 
     # Initiate SIFT detector
     sift = cv.SIFT_create()
@@ -52,89 +68,70 @@ def first_method():
                        singlePointColor=None,
                        matchesMask=matchesMask,     # draw only inliers
                        flags=2)
-
+    # Img 1 to show
     img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-    plt.imshow(img3, 'gray')
+    plt.imshow(img3)
+    plt.title('1')
     plt.show()
 
+    # Img 2 to show
+    roi = cut_out_rectangle(img2, dst)
+    plt.imshow(roi, 'gray')
+    plt.title('2')
+    plt.show()
 
-def second_method():
-    parser = argparse.ArgumentParser(description='Code for Feature Matching with FLANN tutorial.')
-    parser.add_argument('--input1', help='Path to input image 1.', default='box.png')
-    parser.add_argument('--input2', help='Path to input image 2.', default='box_in_scene.png')
-    args = parser.parse_args()
+    dst = np.array([np.int32(dst)[0][0], np.int32(dst)[1][0], np.int32(dst)[2][0], np.int32(dst)[3][0]])
+    src = np.array([[0, 0], [0, 420], [669, 420], [669, 0]])
 
-    img_object = cv.imread('pvi_cv09/obcansky_prukaz_cr_sablona_2012_2014.png', cv.IMREAD_GRAYSCALE)
-    img_scene = cv.imread('pvi_cv09/TA10_01.jpg', cv.IMREAD_GRAYSCALE)
+    tform3 = transform.ProjectiveTransform()
+    tform3.estimate(src, dst)
+    warped = transform.warp(img_positioned, tform3, output_shape=(420, 669))    # Detail of cut out
+    # Img 3 to show
+    plt.imshow(warped, cmap=plt.cm.gray)
+    plt.title('zarovnany prukaz')
+    plt.show()
+    # create face, name, surname
+    face, name, surname = cutout_face_name_surname(warped)
+    """Define Figure3"""
+    fig = plt.figure(figsize=(10, 7))
+    rows = 1
+    columns = 3
+    # 1st position
+    fig.add_subplot(rows, columns, 1)
+    plt.imshow(face, 'gray')
+    plt.title('foto')
+    # 2nd position
+    fig.add_subplot(rows, columns, 2)
+    plt.imshow(surname, 'gray')
+    plt.title('prijmeni')
+    # 3rd position
+    fig.add_subplot(rows, columns, 3)
+    plt.imshow(name, 'gray')
+    plt.title('jmeno')
+    plt.show()
 
-    if img_object is None or img_scene is None:
-        print('Could not open or find the images!')
-        exit(0)
-
-    # -- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-    minHessian = 400
-    detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
-    keypoints_obj, descriptors_obj = detector.detectAndCompute(img_object, None)
-    keypoints_scene, descriptors_scene = detector.detectAndCompute(img_scene, None)
-
-    # -- Step 2: Matching descriptor vectors with a FLANN based matcher
-    # Since SURF is a floating-point descriptor NORM_L2 is used
-    matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
-    knn_matches = matcher.knnMatch(descriptors_obj, descriptors_scene, 2)
-
-    # -- Filter matches using the Lowe's ratio test
-    ratio_thresh = 0.75
-    good_matches = []
-    for m, n in knn_matches:
-        if m.distance < ratio_thresh * n.distance:
-            good_matches.append(m)
-
-    # -- Draw matches
-    img_matches = np.empty((max(img_object.shape[0], img_scene.shape[0]), img_object.shape[1] + img_scene.shape[1], 3),
-                           dtype=np.uint8)
-
-    cv.drawMatches(img_object, keypoints_obj, img_scene, keypoints_scene, good_matches, img_matches,
-                   flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-    # -- Localize the object
-    obj = np.empty((len(good_matches), 2), dtype=np.float32)
-    scene = np.empty((len(good_matches), 2), dtype=np.float32)
-    for i in range(len(good_matches)):
-        # -- Get the keypoints from the good matches
-        obj[i, 0] = keypoints_obj[good_matches[i].queryIdx].pt[0]
-        obj[i, 1] = keypoints_obj[good_matches[i].queryIdx].pt[1]
-        scene[i, 0] = keypoints_scene[good_matches[i].trainIdx].pt[0]
-        scene[i, 1] = keypoints_scene[good_matches[i].trainIdx].pt[1]
-
-    H, _ = cv.findHomography(obj, scene, cv.RANSAC)
-
-    # -- Get the corners from the image_1 ( the object to be "detected" )
-    obj_corners = np.empty((4, 1, 2), dtype=np.float32)
-    obj_corners[0, 0, 0] = 0
-    obj_corners[0, 0, 1] = 0
-    obj_corners[1, 0, 0] = img_object.shape[1]
-    obj_corners[1, 0, 1] = 0
-    obj_corners[2, 0, 0] = img_object.shape[1]
-    obj_corners[2, 0, 1] = img_object.shape[0]
-    obj_corners[3, 0, 0] = 0
-    obj_corners[3, 0, 1] = img_object.shape[0]
-
-    scene_corners = cv.perspectiveTransform(obj_corners, H)
-
-    # -- Draw lines between the corners (the mapped object in the scene - image_2 )
-    cv.line(img_matches, (int(scene_corners[0, 0, 0] + img_object.shape[1]), int(scene_corners[0, 0, 1])), \
-            (int(scene_corners[1, 0, 0] + img_object.shape[1]), int(scene_corners[1, 0, 1])), (0, 255, 0), 4)
-    cv.line(img_matches, (int(scene_corners[1, 0, 0] + img_object.shape[1]), int(scene_corners[1, 0, 1])), \
-            (int(scene_corners[2, 0, 0] + img_object.shape[1]), int(scene_corners[2, 0, 1])), (0, 255, 0), 4)
-    cv.line(img_matches, (int(scene_corners[2, 0, 0] + img_object.shape[1]), int(scene_corners[2, 0, 1])), \
-            (int(scene_corners[3, 0, 0] + img_object.shape[1]), int(scene_corners[3, 0, 1])), (0, 255, 0), 4)
-    cv.line(img_matches, (int(scene_corners[3, 0, 0] + img_object.shape[1]), int(scene_corners[3, 0, 1])), \
-            (int(scene_corners[0, 0, 0] + img_object.shape[1]), int(scene_corners[0, 0, 1])), (0, 255, 0), 4)
-
-    # -- Show detected matches
-    cv.imshow('Good Matches & Object detection', img_matches)
-    cv.waitKey()
+    reader = easyocr.Reader(['cs'], gpu=False)
+    scaled_name = (name * 255).astype('uint8')
+    text_name = reader.readtext(scaled_name, detail=0)      # .lower()
+    text_name = 'jmeno: ' + text_name[0].lower()
+    scaled_surname = (surname * 255).astype('uint8')
+    text_surname = reader.readtext(scaled_surname, detail=0)
+    text_surname = 'prijmeni: ' + text_surname[0].lower()
+    face = cv.putText(img=face,
+                      text=text_name,
+                      org=(10, 15),
+                      fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                      fontScale=0.4,
+                      color=(0, 0, 255),
+                      thickness=1)
+    face = cv.putText(img=face,
+                      text=text_surname,
+                      org=(10, 30),
+                      fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                      fontScale=0.4,
+                      color=(0, 0, 255),
+                      thickness=1)
+    cv.imwrite('cv9_result.jpg', face)
 
 
 def main():
